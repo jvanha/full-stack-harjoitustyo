f465e3456dfd482c1086cf50575c84657b9b3357
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2020-10-19 17:03:04 +0300

    2. paiva aloitus

--- chess-frontend/src/App.js
+++ chess-frontend/src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Board from './components/Board'
-const squares = [
-  [[1,1],[1,2],[1,3]],
-  [[2,1],[2,2],[2,3]],
-  [[3,1],[3,2],[3,3]],
-  
-] 
+const squares = [...Array(64).keys()] 
 function App() {
   return (
     <div>
