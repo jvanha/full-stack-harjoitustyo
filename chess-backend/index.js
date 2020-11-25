f1ffd63f7201277eb19285bbdbe026ee0270ee62
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2020-11-25 18:38:48 +0200

    12. paiva lopetus

--- chess-backend/index.js
+++ chess-backend/index.js
@@ -1,3 +1,73 @@
+const { ApolloServer, gql, PubSub, withFilter } = require('apollo-server')
+
+const pubsub = new PubSub()
+
+const typeDefs = gql`
+  type Piece {
+    pieceType: String!
+    color: String!
+  }
+
+  type Square {
+    position: Int!
+    piece: Piece
+  }
+
+  type Move {
+    from: Int!
+    to: Int!
+  }
+  type Query {
+    hah: String!
+  }
+  type Mutation {
+    makeAMove(opponentId: String! from: Int! to: Int!): Move
+  }
+  type Subscription {
+    moveMade(playerId: String): Move
+  }
+`
+
+
+
+
+
+const resolvers = {
+  Query: {
+    hah: () => 'hah'
+  },
+  Mutation: {
+    makeAMove: (root, args) => {
+      //const {opponentId, from, to} = args
+      const from = args.from
+      const to = args.to
+      const move = { from, to }
+      console.log(move)
+      const opponentId = args.opponentId
+      pubsub.publish('MOVE_MADE', { opponentId, moveMade: move })
+      return move
+    }
+  },
+  Subscription: {
+    moveMade: {
+      subscribe: withFilter(() => pubsub.asyncIterator('MOVE_MADE'), (payload, variables) => {
+        return payload.opponentId === variables.playerId
+      })
+    //  subscribe: () => pubsub.asyncIterator(['MOVE_MADE'])
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
+
+/*
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
@@ -13,5 +83,6 @@ const server = new ApolloServer([
 ])
 
 server.listen().then(({ url }) => {
-  console.log('Server ready at')
+  console.log('Server ready at', url)
 })
+*/
