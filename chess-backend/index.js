e6f043e3dcfa6918a5b8dec516dfe077fead1906
Joonas Vanhalakka <joonas.vanhalakka@helsinki.fi>
2020-11-26 18:00:29 +0200

    13. paiva aloitus

--- chess-backend/index.js
+++ chess-backend/index.js
@@ -1,8 +1,31 @@
-const { ApolloServer, gql, PubSub, withFilter } = require('apollo-server')
-
+const { ApolloServer, gql, PubSub, withFilter, UserInputError } = require('apollo-server')
+const mongoose = require('mongoose')
 const pubsub = new PubSub()
+const jwt = require('jsonwebtoken')
+const User = require('./models/user')
+
+const JWT_SECRET = 'suusialas'
+const MONGODB_URI = 'mongodb+srv://fullstack:poalkj@cluster0.j1rcu.mongodb.net/chess?retryWrites=true&w=majority'
+console.log('connecting to', MONGODB_URI)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+
+  })
 
 const typeDefs = gql`
+  type User {
+    username: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Piece {
     pieceType: String!
     color: String!
@@ -17,33 +40,58 @@ const typeDefs = gql`
     from: Int!
     to: Int!
   }
+
   type Query {
     hah: String!
   }
+
   type Mutation {
-    makeAMove(opponentId: String! from: Int! to: Int!): Move
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+    makeAMove(
+      opponentId: String!
+      from: Int!
+      to: Int!
+      ): Move
   }
   type Subscription {
     moveMade(playerId: String): Move
   }
 `
 
-
-
-
-
 const resolvers = {
   Query: {
     hah: () => 'hah'
   },
   Mutation: {
+    createUser: (root, args) => {
+      const user = new User({ ...args })
+      return user.save()
+    },
+    login: async (root, args) => {
+      const { username, password } = args
+      const user = await User.findOne({ username })
+      if (!user || password != '1234') {
+        throw new UserInputError('Bad credentials')
+      }
+      const userForToken = {
+        username: user.username,
+        id:  user._id
+      }
+      return { value: jwt.sign(userForToken, JWT_SECRET)}
+    },
     makeAMove: (root, args) => {
-      //const {opponentId, from, to} = args
-      const from = args.from
-      const to = args.to
+      const { opponentId, from, to } = args
+      //const from = args.from
+      //const to = args.to
       const move = { from, to }
       console.log(move)
-      const opponentId = args.opponentId
+      //const opponentId = args.opponentId
       pubsub.publish('MOVE_MADE', { opponentId, moveMade: move })
       return move
     }
